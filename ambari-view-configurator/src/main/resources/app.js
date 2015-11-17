0919ac27180800ef35887c53357ce194448c4f75
Viacheslav Pryimak <vpryimak@softserveinc.com>
2015-11-17 13:02:41 +0200

    first commit

---
+++ ambari-view-configurator/src/main/resources/app.js
@@ -0,0 +1,301 @@
+var VERSION = "0.1.0"
+
+var app = angular.module('MesosMetricsApp', ['nvd3']);
+
+app.filter('toGb', function () {
+    return function (input) {
+        return (input / 1024).toFixed(2);
+    }
+});
+
+app.controller('MetricsController', function ($scope, $http, $interval, $q) {
+    $scope.errors = null;
+    $scope.masterList = [];
+    $scope.slaveList = [];
+
+    $scope.slaveMetrics = [];
+    $scope.masterStates = [];
+    $scope.masterDataCpu = [];
+    $scope.masterDataMem = [];
+    $scope.masterDataDisk = [];
+    $scope.masteterDataCpuAbsolute = [];
+    $scope.masterDataMemAbsolute = [];
+    $scope.masterDataDiskAbsolute = [];
+
+    $scope.slaveDataCpu = {};
+    $scope.slaveDataMem = {};
+    $scope.slaveDataDisk = {};
+    $scope.slaveData = [];
+
+    $scope.activeMaster = null;
+    $scope.clusterName = null;
+
+    $scope.d3Config = {
+        extended: true,
+        autorefresh: true,
+        debounce: 10
+    }
+
+    $scope.optionsMaster = {
+        chart: {
+            type: 'pieChart',
+            height: 150,
+            donut: true,
+            x: function (d) {
+                return d.name;
+            },
+            y: function (d) {
+                return d.size;
+            },
+            showLabels: false,
+            transitionDuration: 500,
+        }
+    };
+
+    $scope.optionsSlave = {
+        chart: {
+            type: 'pieChart',
+            height: 150,
+            x: function (d) {
+                return d.name;
+            },
+            y: function (d) {
+                return d.size;
+            },
+            showLegend: false,
+            showLabels: false,
+
+            transitionDuration: 500,
+            legend: {
+                margin: {
+                    top: 5,
+                    right: 140,
+                    bottom: 5,
+                    left: 0
+                }
+            }
+        }
+    };
+
+    $scope.callAtInterval = function () {
+        $scope.getMesosMetrics();
+    };
+
+    // Get metrics for elected master host
+    $scope.getMetricsForMaster = function (clusterName, masterHost) {
+        // /api/v1/views/MESOSMETRICS/versions/0.1.0/instances/mesos/resources/mesos/metrics/mesos-master-01/5050
+        $http.get('/api/v1/views/MESOSMETRICS/versions/' + VERSION + '/instances/mesos/resources/mesos/metrics/' + masterHost + '/5050', {cache: true})
+            .success(function (data) {
+                items = JSON.parse(data);
+                console.log("masterMetrics -> " + data)
+                if (items.metrics["master/elected"] == 1.0) {
+                    $scope.activeMaster = items.id
+                    var usedCpu = (items.metrics["master/cpus_percent"] * 100).toFixed(1);
+                    var freeCpu = 100 - usedCpu;
+                    //var usedCpu = 100-20
+                    //var freeCpu = 100 - 80;
+                    $scope.masterDataCpu =
+                        [
+                            {
+                                name: "Used(" + usedCpu + "%)",
+                                size: usedCpu
+                            },
+                            {
+                                name: "Free(" + freeCpu + "%)",
+                                size: freeCpu
+                            }
+                        ];
+                    $scope.masterDataCpuAbsolute = [
+                        {
+                            name: "Total",
+                            size: items.metrics["master/cpus_total"]
+                        },
+                        {
+                            name: "Used",
+                            size: items.metrics["master/cpus_used"]
+                        },
+                        {
+                            name: "Free",
+                            size: items.metrics["master/cpus_total"] - items.metrics["master/cpus_used"]
+                        }
+                    ]
+                    var usedMem = (items.metrics["master/mem_percent"] * 100).toFixed(1);
+                    var freeMem = 100 - usedMem;
+                    //var usedMem = 40;
+                    //var freeMem = 60;
+                    $scope.masterDataMem = [
+                        {
+                            "name": "Used(" + usedMem + "%)",
+                            "size": usedMem,
+                        },
+                        {
+                            "name": "Free(" + (freeMem) + "%)",
+                            "size": freeMem,
+                        }
+                    ];
+                    $scope.masterDataMemAbsolute = [
+                        {
+                            name: "Total",
+                            size: (items.metrics["master/mem_total"] / 1024).toFixed(2)
+                        },
+                        {
+                            name: "Used",
+                            size: (items.metrics["master/mem_used"] / 1024).toFixed(2)
+                        },
+                        {
+                            name: "Free",
+                            size: ((items.metrics["master/mem_total"] - items.metrics["master/mem_used"]) / 1024).toFixed(2)
+                        }
+                    ];
+                    var usedDisk = (items.metrics["master/disk_percent"] * 100).toFixed(1);
+                    var freeDisk = 100 - usedDisk;
+                    //var usedDisk = 15;
+                    //var freeDisk = 85;
+                    $scope.masterDataDisk = [
+                        {
+                            "name": "Used(" + usedDisk + "%)",
+                            "size": usedDisk,
+                        },
+                        {
+                            "name": "Free(" + freeDisk + "%)",
+                            "size": freeDisk,
+                        }
+                    ];
+                    $scope.masterDataDiskAbsolute = [
+                        {
+                            name: "Total",
+                            size: (items.metrics["master/disk_total"] / 1024).toFixed(2)
+                        },
+                        {
+                            name: "Used",
+                            size: (items.metrics["master/disk_used"] / 1024).toFixed(2)
+                        },
+                        {
+                            name: "Free",
+                            size: ((items.metrics["master/disk_total"] - items.metrics["master/disk_used"]) / 1024).toFixed(2)
+                        }
+                    ];
+                }
+            })
+            .error(function (data) {
+                console.log('Error: ' + JSON.stringify(data));
+            })
+    }
+
+    // Get metrics for slave host
+    $scope.getMetricsForSlave = function (slaveHost) {
+        $http.get('/api/v1/views/MESOSMETRICS/versions/' + VERSION + '/instances/mesos/resources/mesos/metrics/' + slaveHost + '/5051', {cache: true})
+            .success(function (data) {
+                items = JSON.parse(data);
+
+                $scope.slaveData.push(items);
+
+                $scope.slaveDataCpu.push({
+                    id: slaveHost,
+                    "metrics": [
+                        {
+                            name: "Used",
+                            size: items.metrics["slave/cpus_used"]
+                        },
+                        {
+                            name: "Free",
+                            size: items.metrics["slave/cpus_total"] - items.metrics["slave/cpus_used"]
+                        }
+                    ]
+                });
+
+                $scope.slaveDataMem.push({
+                    id: slaveHost,
+                    "metrics": [
+                        {
+                            name: "Used",
+                            size: (items.metrics["slave/mem_used"] / 1024).toFixed(2)
+                        },
+                        {
+                            name: "Free",
+                            size: ((items.metrics["slave/mem_total"] - items.metrics["slave/mem_used"]) / 1024).toFixed(2)
+                        }
+                    ]
+                });
+
+                $scope.slaveDataDisk.push({
+                    id: slaveHost,
+                    "metrics": [
+                        {
+                            name: "Used",
+                            size: (items.metrics["slave/disk_used"] / 1024).toFixed(2)
+                        },
+                        {
+                            name: "Free",
+                            size: ((items.metrics["slave/disk_total"] - items.metrics["slave/disk_used"]) / 1024).toFixed(2)
+                        }
+                    ]
+                });
+                console.log("$scope.slaveData -> " + JSON.stringify($scope.slaveData));
+                console.log("$scope.slaveDataCpu -> " + JSON.stringify($scope.slaveDataCpu));
+            })
+            .error(function (data) {
+                console.log('Error: ' + JSON.stringify(data));
+            })
+    }
+
+    $scope.getMesosMetrics = function () {
+
+        /*
+         * Get clusterName from master
+         */
+        $q.all({
+            master: $http.get('/api/v1/clusters')
+        }).then(function (masterData) {
+            $scope.clusterName = masterData.master.data.items[0].Clusters.cluster_name;
+
+            /*
+             * Get mesos master hosts
+             */
+            $q.all({
+                mesosMaster: $http.get('/api/v1/clusters/' + $scope.clusterName + '/services/MESOS/components/MESOS_MASTER')
+            }).then(function (mesosMasterData) {
+
+                console.log("mesosMasterData -> " + JSON.stringify(mesosMasterData))
+
+                var masterItems = mesosMasterData.mesosMaster.data.host_components;
+                $scope.masterList = [];
+
+                for (var i = 0; i < masterItems.length; i++) {
+                    $scope.masterList.push(masterItems[i].HostRoles.host_name);
+                    // Call draw function for masters
+                    $scope.getMetricsForMaster($scope.clusterName, masterItems[i].HostRoles.host_name)
+                }
+
+                console.log("masterList -> " + $scope.masterList);
+            })
+
+            /*
+             * Get mesos slave hosts
+             */
+            $q.all({
+                mesosSlave: $http.get('/api/v1/clusters/' + $scope.clusterName + '/services/MESOS/components/MESOS_SLAVE')
+            }).then(function (mesosSlaveData) {
+
+                var slaveItems = mesosSlaveData.mesosSlave.data.host_components;
+                $scope.slaveList = [];
+                $scope.slaveData = [];
+                $scope.slaveDataCpu = [];
+                $scope.slaveDataMem = [];
+                $scope.slaveDataDisk = [];
+                for (var i = 0; i < slaveItems.length; i++) {
+                    $scope.slaveList.push(slaveItems[i].HostRoles.host_name);
+                    // Call draw function for slaves
+                    $scope.getMetricsForSlave(slaveItems[i].HostRoles.host_name)
+                }
+
+                console.log("slaveList: " + $scope.slaveList);
+            })
+        })
+    }
+
+    $interval(function () {
+        $scope.callAtInterval();
+    }, 10000);
+
+});
---
+++ ambari-view-metrics/src/main/java/org/apache/ambari/view/metrics/MesosService.java
@@ -0,0 +1,91 @@
+package org.apache.ambari.view.metrics;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.ambari.view.ViewContext;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.utils.URIBuilder;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Andrii Sabitov on 11/4/15.
+ */
+
+public class MesosService {
+
+    @GET
+    @Path("/metrics/{nodeName}/{portName}")
+    @Produces({"text/plain", "application/json"})
+    public Response getMetrics(@Context HttpHeaders headers, @Context UriInfo ui, @PathParam("nodeName") String nodeName, @PathParam("portName") int portName) {
+        return getResponse(nodeName, portName, "/metrics/snapshot");
+    }
+
+
+    @GET
+    @Path("/state/{nodeName}/{portName}")
+    @Produces({"text/plain", "application/json"})
+    public Response getState(@Context HttpHeaders headers, @Context UriInfo ui, @PathParam("nodeName") String nodeName, @PathParam("portName") int portName) {
+        return getResponse(nodeName, portName, "/state.json");
+    }
+
+    private Response getResponse(String nodeName, int portName, String uri) {
+        MesosNodeResource mesosResource = new MesosNodeResource();
+        try {
+            mesosResource.setId(nodeName);
+            mesosResource.setMetrics(getJsonObject(nodeName, portName, uri));
+            mesosResource.setEnabled(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        String response = new Gson().toJson(mesosResource);
+        return Response.ok(response).build();
+    }
+
+    private Map<String, Object> getJsonObject(String nodeName, int port, String uri) throws IOException {
+        URIBuilder uriBuilder = new URIBuilder();
+        uriBuilder.setScheme("http");
+        uriBuilder.setHost(nodeName);
+        uriBuilder.setPort(port);
+        uriBuilder.setPath(uri);
+        String url = uriBuilder.toString();
+
+        InputStream in = readFrom(url);
+        try {
+            Type mapType = new TypeToken<Map<String, Object>>() {
+            }.getType();
+            return new Gson().fromJson(IOUtils.toString(in, "UTF-8"), mapType);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            in.close();
+        }
+        return new HashMap<String, Object>();
+    }
+
+    private static InputStream readFrom(String spec) throws IOException {
+        URLConnection connection = new URL(spec).openConnection();
+        connection.setConnectTimeout(5000);
+        connection.setDoOutput(true);
+        return connection.getInputStream();
+    }
+
+
+
+
+}
