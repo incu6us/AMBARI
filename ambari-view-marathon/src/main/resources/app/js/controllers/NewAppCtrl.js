5c231f7e48ba6ce2eed726be8b30fc2df1b7e219
anikitenko <alexvwan@gmail.com>
2016-01-12 20:18:06 +0200

    Continue working on modal window for creating application

--- ambari-view-marathon/src/main/resources/app/js/controllers/NewAppCtrl.js
+++ ambari-view-marathon/src/main/resources/app/js/controllers/NewAppCtrl.js
@@ -5,10 +5,12 @@
   	.module('MarathonApp')
   	.controller('NewAppCtrl', NewAppCtrl);
 
-  	NewAppCtrl.$inject = ['$scope', '$mdDialog', '$mdMedia', 'ControlButtonsCtrl'];
+  	NewAppCtrl.$inject = ['$scope', '$mdDialog', '$mdMedia'];
 
-  	function NewAppCtrl ($scope, $mdDialog, $mdMedia, ControlButtonsCtrl) {
+  	function NewAppCtrl ($scope, $mdDialog, $mdMedia) {
 	    $scope.status = '  ';
+	    $scope.docker_networks = [{ netid: 'HOST', value: 'Host' }, { netid: 'BRIDGE', value: 'Bridged' }];
+	    $scope.docker_protocols = [{ netid: 'tcp', value: 'tcp' }, { netid: 'udp', value: 'udp' }];
 	    $scope.cancel = function() {
 	        $mdDialog.cancel();
 	    };
