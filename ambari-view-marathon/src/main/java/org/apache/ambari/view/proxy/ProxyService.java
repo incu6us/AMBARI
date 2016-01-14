7f2af3f2c1cdc2d6eda72afc345a24d5f2727f0f
anikitenko <alexvwan@gmail.com>
2016-01-14 18:48:22 +0200

    Added PUT & DELETE methods to proxy

--- ambari-view-marathon/src/main/java/org/apache/ambari/view/proxy/ProxyService.java
+++ ambari-view-marathon/src/main/java/org/apache/ambari/view/proxy/ProxyService.java
@@ -9,6 +9,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -34,6 +36,8 @@ public class ProxyService {
      */
     @GET
     @POST
+    @PUT
+    @DELETE
     @Path("/json")
     @Produces(MediaType.APPLICATION_JSON)
     public JSONObject getJson(@QueryParam("url") String url) {
