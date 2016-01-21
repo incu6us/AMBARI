16dff577a79258fee7642ff21bcc02abff5a837a
anikitenko <alexvwan@gmail.com>
2016-01-21 15:09:06 +0200

    Added dialog windows

--- ambari-view-marathon/src/main/resources/app/components/apps-deployments/apps-deployments.ctrl.js
+++ ambari-view-marathon/src/main/resources/app/components/apps-deployments/apps-deployments.ctrl.js
@@ -5,9 +5,9 @@
     	.module('MarathonApp')
     	.controller('AppsDeploymentsCtrl', AppsDeploymentsCtrl);
 
-    	AppsDeploymentsCtrl.$inject = ['$timeout', '$location', 'AppDeploymentFactory', 'HostNameFactory'];
+    	AppsDeploymentsCtrl.$inject = ['$timeout', '$location', '$mdDialog', 'AppDeploymentFactory', 'HostNameFactory'];
 
-    	function AppsDeploymentsCtrl ($timeout, $location, AppDeploymentFactory, HostNameFactory) {
+    	function AppsDeploymentsCtrl ($timeout, $location, $mdDialog, AppDeploymentFactory, HostNameFactory) {
 	        var vm = this;
 
 	        vm.hostName = '';
@@ -27,22 +27,62 @@
 		            });
 	        }
 
+	        vm.showAppInfo = showAppInfo;
+	        vm.stopDeploy = stopDeploy;
+	        vm.rollbackDeploy = rollbackDeploy;
+
 	        function showAppInfo (appId) {
 	            $location.path('/apps/' + encodeURIComponent(appId));
 	        }
 
 	        function stopDeploy (deployId) {
-                AppDeploymentFactory.stop(vm.hostName, deployId)
-	            	.then( function(response) {
-		                console.log(response);
-		            });
+	            vm.deployId = deployId;
+                $mdDialog.show({
+                		            templateUrl: 'app/components/apps-deployments/apps-deployments-stop.tpl.html',
+                		            controller: 'AppsDeploymentsCtrl',
+                		            controllerAs: 'appDeploys',
+                		            parent: angular.element(document.querySelector('#content')),
+                		            targetEvent: ev,
+                		            clickOutsideToClose:true
+                		        });
 	        }
 
-	        function rollbackDeploy (deployId) {
-                            AppDeploymentFactory.rollback(vm.hostName, deployId)
+	        vm.submitStop = function submit () {
+	            var deployId = vm.deployId;
+	            AppDeploymentFactory.stop(vm.hostName, deployId)
             	            	.then( function(response) {
+            	            	    $mdDialog.cancel();
             		                console.log(response);
             		            });
+	        }
+
+	        vm.cancelStop = function cancel () {
+                            $mdDialog.cancel();
+                        }
+
+	        function rollbackDeploy (deployId) {
+	            vm.deployId = deployId;
+	            $mdDialog.show({
+                                		            templateUrl: 'app/components/apps-deployments/apps-deployments-rollback.tpl.html',
+                                		            controller: 'AppsDeploymentsCtrl',
+                                		            controllerAs: 'appDeploys',
+                                		            parent: angular.element(document.querySelector('#content')),
+                                		            targetEvent: ev,
+                                		            clickOutsideToClose:true
+                                		        });
             	        }
+
+            vm.submitDestory = function () {
+                var deployId = vm.deployId;
+                AppDeploymentFactory.rollback(vm.hostName, deployId)
+                            	            	.then( function(response) {
+                            	            	    $mdDialog.cancel();
+                            		                console.log(response);
+                            		            });
+            }
+
+            vm.cancelDestory = function () {
+                $mdDialog.cancel();
+            }
     	}
 }());
