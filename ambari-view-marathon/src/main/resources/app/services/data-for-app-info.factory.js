b27d5792eea586cd6be090d13a590f1cdc239ce4
Nikita Yuzhakov <nikke15@yandex.ru>
2016-02-08 11:02:20 +0200

    services improved

--- ambari-view-marathon/src/main/resources/app/services/data-for-app-info.factory.js
+++ ambari-view-marathon/src/main/resources/app/services/data-for-app-info.factory.js
@@ -16,9 +16,9 @@
             // /api/v1/views/MARATHON/versions/0.1.0/instances/marathon/resources/proxy/json?url=http://' + hostName + ':8080/v2/apps/' + appID + '?embed=app.taskStats
 
             // for Brunch server
-            // http://nikke1.github.io/mesos.json
+            // http://nikke1.github.io/hard-data/mesos.json
             function get (hostName, appID) {
-                return $http.get('http://nikke1.github.io/mesos.json')
+                return $http.get('/api/v1/views/MARATHON/versions/0.1.0/instances/marathon/resources/proxy/json?url=http://' + hostName + ':8080/v2/apps/' + appID + '?embed=app.taskStats')
                     .then(successDataForAppInfo, errorDataForAppInfo);
 
                 function successDataForAppInfo(response) {
